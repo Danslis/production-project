d9773301756d2cf1e95078d10804001df8ef802d
Dart <Danslis@yandex.ru>
2023-09-13 21:54:32 +0300

    дев сервер и переменные окружения

--- client/config/build/buildPlugins.ts
+++ client/config/build/buildPlugins.ts
@@ -1,8 +1,7 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 import webpack from "webpack";
-import { BuildOptions } from "./types/config";
-
+import {BuildOptions} from "./types/config";
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
 
