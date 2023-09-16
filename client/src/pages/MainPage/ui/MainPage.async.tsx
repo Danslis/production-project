73f414fda6ca2dfc1b1c58cefe61c69992be6078
Dart <Danslis@yandex.ru>
2023-09-16 22:39:43 +0300

    накидали архитектуру

--- client/src/pages/MainPage/ui/MainPage.async.tsx
+++ client/src/pages/MainPage/ui/MainPage.async.tsx
@@ -2,6 +2,6 @@ import {lazy} from "react";
 
 export const MainPageAsync = lazy(() => new Promise(resolve => {
     // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ ПРОСМОТРА ПОВЕДЕНИЯ!
+    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ПРОСМОТРА ПОВЕДЕНИЯ!
     setTimeout(() => resolve(import('./MainPage')), 1500)
-}))
+}));
