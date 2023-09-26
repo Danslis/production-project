fa09c2e9d49600754167bab9bf886c40f8c55827
Dart <Danslis@yandex.ru>
2023-09-26 23:03:32 +0300

    правки для сборки

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,7 +3,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
+import { Sidebar } from 'widgets/SideBar';
+
 
 function App() {
     const { theme } = useTheme();
