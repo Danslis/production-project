d69b2bd0326ff10f92165a74542b283673414110
Dart <Danslis@yandex.ru>
2023-09-27 22:55:06 +0300

    пример работы с redux cчетчик

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,7 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/SideBar';
+import { Sidebar } from 'widgets/Sidebar';
 
 function App() {
     const { theme } = useTheme();
